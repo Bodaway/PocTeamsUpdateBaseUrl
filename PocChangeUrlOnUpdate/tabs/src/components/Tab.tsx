917e9958afa0e5226e66a4b9d8e14b741b9d9647
Michel Bonenfant <mbonenfant@witivio.com>
2021-06-30 14:50:11 +0200

    .

---
+++ PocChangeUrlOnUpdate/tabs/src/components/Tab.tsx
@@ -0,0 +1,7 @@
+import { Welcome } from "./sample/Welcome";
+
+export default function Tab() {
+  return <div>
+    <Welcome />
+  </div>;
+}
---
+++ PocChangeUrlOnUpdate/tabs/src/components/sample/Welcome.tsx
@@ -0,0 +1,9 @@
+import "./Welcome.css";
+
+export function Welcome() {
+  return (
+    <div className="welcome page">
+        <h1 className="center">APP V2</h1>
+    </div>
+  );
+}
